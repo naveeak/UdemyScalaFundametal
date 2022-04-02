d6b98edc2cfbcd1404d9c637790d4d6783cfbb79
ak <ak7195664@gmail.com>
2022-04-02 22:50:46 +0530

    enum class commented the code

---
+++ src/com/udemy/course/lectures/part2oop/Enums.scala
@@ -0,0 +1,44 @@
+package com.udemy.course.lectures.part2oop
+
+import java.security.Permissions
+
+object Enums {
+
+  //datatype much like class only works in sacal 3
+  /*enum Permissions{
+    case READ,WRITE,EXECUTE,NONE
+
+    CAN ALSO HAVE METHods
+    def appDocument():Unit={
+    //do something
+    }
+
+  }
+
+  contrictir args
+  enum PermissionwBits(bits:Int){
+  case READ extends PermissionwBits(1)
+  ....
+  }
+
+enum also have companion object
+
+object PermissionwBits{
+def fromBits(bits:Int):PermissionwBits ={//return what you need}
+}
+
+
+   val permission:Permissions = Permissions.READ
+    permission.appDocument()
+
+    //standard API
+val somePermissionsOrdinal = permission.ordinal //returns the index
+PermissionwBits.value//return all the value
+PermissionwBits.valueOf("READ") // will retunr the read enum
+
+    println(somePermissionsOrdinal)
+
+
+   */
+
+}
